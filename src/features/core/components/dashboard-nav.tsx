051ea4c3a4271495fb922c90e9397200716db3e7
Mind <alwl1@hotmail.com>
2024-07-22 19:33:14 -0300

    refactor(dashboard nav): remove hardcoded button title from logout button

--- src/features/core/components/dashboard-nav.tsx
+++ src/features/core/components/dashboard-nav.tsx
@@ -6,91 +6,76 @@ import { cn } from "@/features/core/lib/utils";
 import { NavItem } from "@/features/core/types";
 import { Dispatch, SetStateAction } from "react";
 import { useSidebar } from "@/features/core/hooks/useSidebar";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "./ui/tooltip";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
 import { useAuthStore } from "@/features/auth/stores";
 import { ValidRoles } from "@/features/auth/types";
 
 interface DashboardNavProps {
-  items: NavItem[];
-  setOpen?: Dispatch<SetStateAction<boolean>>;
-  isMobileNav?: boolean;
+	items: NavItem[];
+	setOpen?: Dispatch<SetStateAction<boolean>>;
+	isMobileNav?: boolean;
 }
 
-export function DashboardNav({
-  items,
-  setOpen,
-  isMobileNav = false,
-}: DashboardNavProps) {
-  // const path = usePathname();
+export function DashboardNav({ items, setOpen, isMobileNav = false }: DashboardNavProps) {
+	// const path = usePathname();
+	const userRole = useAuthStore((state) => state.userRole) || [];
 
-  const logoutUser = useAuthStore((state) => state.logoutUser);
-  const userRole = useAuthStore((state) => state.userRole) || [];
+	const location = useLocation();
+	const path = location.pathname;
+	const { isMinimized } = useSidebar();
 
-  const location = useLocation();
-  const path = location.pathname;
-  const { isMinimized } = useSidebar();
+	if (!items?.length) {
+		return null;
+	}
 
-  if (!items?.length) {
-    return null;
-  }
+	console.log("isActive", isMobileNav, isMinimized);
 
-  console.log("isActive", isMobileNav, isMinimized);
+	return (
+		<nav className="grid items-start gap-2">
+			<TooltipProvider>
+				{items.map((item) => {
+					const isVisible =
+						userRole.includes(ValidRoles.admin) ||
+						!item.validRole ||
+						item.validRole.some((role) => userRole.includes(role));
 
-  return (
-    <nav className="grid items-start gap-2">
-      <TooltipProvider>
-        {items.map((item, index) => {
-          const isVisible =
-            userRole.includes(ValidRoles.admin) ||
-            !item.validRole ||
-            item.validRole.some((role) => userRole.includes(role));
+					const Icon = Icons[item.icon || "arrowRight"];
+					return (
+						isVisible && (
+							<Tooltip key={Symbol(item.title).description}>
+								<TooltipTrigger asChild>
+									<Link
+										to={item.disabled ? "/" : item.href ?? "#"}
+										className={cn(
+											"flex items-center gap-2 overflow-hidden rounded-md py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
+											path === item.href ? "bg-accent" : "transparent",
+											item.disabled && "cursor-not-allowed opacity-80",
+										)}
+										onClick={(e) => {
+											item.onClick?.(e);
+											setOpen?.(false);
+										}}>
+										<Icon className="ml-3 size-5" />
 
-          const Icon = Icons[item.icon || "arrowRight"];
-          return (
-            item.href &&
-            isVisible && (
-              <Tooltip key={index}>
-                <TooltipTrigger asChild>
-                  <Link
-                    to={item.disabled ? "/" : item.href}
-                    className={cn(
-                      "flex items-center gap-2 overflow-hidden rounded-md py-2 text-sm font-medium hover:bg-accent hover:text-accent-foreground",
-                      path === item.href ? "bg-accent" : "transparent",
-                      item.disabled && "cursor-not-allowed opacity-80"
-                    )}
-                    onClick={() => {
-                      if (item.title === "Salir") {
-                        logoutUser();
-                      } else if (setOpen) {
-                        setOpen(false);
-                      }
-                    }}>
-                    <Icon className={`ml-3 size-5`} />
-
-                    {isMobileNav || (!isMinimized && !isMobileNav) ? (
-                      <span className="mr-2 truncate">{item.title}</span>
-                    ) : (
-                      ""
-                    )}
-                  </Link>
-                </TooltipTrigger>
-                <TooltipContent
-                  align="center"
-                  side="right"
-                  sideOffset={8}
-                  className={!isMinimized ? "hidden" : "inline-block"}>
-                  {item.title}
-                </TooltipContent>
-              </Tooltip>
-            )
-          );
-        })}
-      </TooltipProvider>
-    </nav>
-  );
+										{isMobileNav || (!isMinimized && !isMobileNav) ? (
+											<span className="mr-2 truncate">{item.title}</span>
+										) : (
+											""
+										)}
+									</Link>
+								</TooltipTrigger>
+								<TooltipContent
+									align="center"
+									side="right"
+									sideOffset={8}
+									className={!isMinimized ? "hidden" : "inline-block"}>
+									{item.title}
+								</TooltipContent>
+							</Tooltip>
+						)
+					);
+				})}
+			</TooltipProvider>
+		</nav>
+	);
 }
--- src/features/core/constants/data.ts
+++ src/features/core/constants/data.ts
@@ -1,194 +1,184 @@
+import type { NavItem } from "@/features/core/types";
+
 import { ValidRoles } from "@/features/auth/types";
-import { NavItem } from "@/features/core/types";
+import { useAuthStore } from "@/features/auth/stores";
 
 export type User = {
-  id: number;
-  name: string;
-  company: string;
-  role: string;
-  verified: boolean;
-  status: string;
+	id: number;
+	name: string;
+	company: string;
+	role: string;
+	verified: boolean;
+	status: string;
 };
 export const users: User[] = [
-  {
-    id: 1,
-    name: "Candice Schiner",
-    company: "Dell",
-    role: "Frontend Developer",
-    verified: false,
-    status: "Active",
-  },
-  {
-    id: 2,
-    name: "John Doe",
-    company: "TechCorp",
-    role: "Backend Developer",
-    verified: true,
-    status: "Active",
-  },
-  {
-    id: 3,
-    name: "Alice Johnson",
-    company: "WebTech",
-    role: "UI Designer",
-    verified: true,
-    status: "Active",
-  },
-  {
-    id: 4,
-    name: "David Smith",
-    company: "Innovate Inc.",
-    role: "Fullstack Developer",
-    verified: false,
-    status: "Inactive",
-  },
-  {
-    id: 5,
-    name: "Emma Wilson",
-    company: "TechGuru",
-    role: "Product Manager",
-    verified: true,
-    status: "Active",
-  },
-  {
-    id: 6,
-    name: "James Brown",
-    company: "CodeGenius",
-    role: "QA Engineer",
-    verified: false,
-    status: "Active",
-  },
-  {
-    id: 7,
-    name: "Laura White",
-    company: "SoftWorks",
-    role: "UX Designer",
-    verified: true,
-    status: "Active",
-  },
-  {
-    id: 8,
-    name: "Michael Lee",
-    company: "DevCraft",
-    role: "DevOps Engineer",
-    verified: false,
-    status: "Active",
-  },
-  {
-    id: 9,
-    name: "Olivia Green",
-    company: "WebSolutions",
-    role: "Frontend Developer",
-    verified: true,
-    status: "Active",
-  },
-  {
-    id: 10,
-    name: "Robert Taylor",
-    company: "DataTech",
-    role: "Data Analyst",
-    verified: false,
-    status: "Active",
-  },
+	{
+		id: 1,
+		name: "Candice Schiner",
+		company: "Dell",
+		role: "Frontend Developer",
+		verified: false,
+		status: "Active",
+	},
+	{
+		id: 2,
+		name: "John Doe",
+		company: "TechCorp",
+		role: "Backend Developer",
+		verified: true,
+		status: "Active",
+	},
+	{
+		id: 3,
+		name: "Alice Johnson",
+		company: "WebTech",
+		role: "UI Designer",
+		verified: true,
+		status: "Active",
+	},
+	{
+		id: 4,
+		name: "David Smith",
+		company: "Innovate Inc.",
+		role: "Fullstack Developer",
+		verified: false,
+		status: "Inactive",
+	},
+	{
+		id: 5,
+		name: "Emma Wilson",
+		company: "TechGuru",
+		role: "Product Manager",
+		verified: true,
+		status: "Active",
+	},
+	{
+		id: 6,
+		name: "James Brown",
+		company: "CodeGenius",
+		role: "QA Engineer",
+		verified: false,
+		status: "Active",
+	},
+	{
+		id: 7,
+		name: "Laura White",
+		company: "SoftWorks",
+		role: "UX Designer",
+		verified: true,
+		status: "Active",
+	},
+	{
+		id: 8,
+		name: "Michael Lee",
+		company: "DevCraft",
+		role: "DevOps Engineer",
+		verified: false,
+		status: "Active",
+	},
+	{
+		id: 9,
+		name: "Olivia Green",
+		company: "WebSolutions",
+		role: "Frontend Developer",
+		verified: true,
+		status: "Active",
+	},
+	{
+		id: 10,
+		name: "Robert Taylor",
+		company: "DataTech",
+		role: "Data Analyst",
+		verified: false,
+		status: "Active",
+	},
 ];
 
 export type Employee = {
-  id: number;
-  first_name: string;
-  last_name: string;
-  email: string;
-  phone: string;
-  gender: string;
-  date_of_birth: string; // Consider using a proper date type if possible
-  street: string;
-  city: string;
-  state: string;
-  country: string;
-  zipcode: string;
-  longitude?: number; // Optional field
-  latitude?: number; // Optional field
-  job: string;
-  profile_picture?: string | null; // Profile picture can be a string (URL) or null (if no picture)
+	id: number;
+	first_name: string;
+	last_name: string;
+	email: string;
+	phone: string;
+	gender: string;
+	date_of_birth: string; // Consider using a proper date type if possible
+	street: string;
+	city: string;
+	state: string;
+	country: string;
+	zipcode: string;
+	longitude?: number; // Optional field
+	latitude?: number; // Optional field
+	job: string;
+	profile_picture?: string | null; // Profile picture can be a string (URL) or null (if no picture)
 };
 
 export const navItems: NavItem[] = [
-  {
-    title: "Athenea",
-    href: "/dashboard",
-    icon: "dashboard",
-    label: "Dashboard",
-    validRole: [
-      ValidRoles.admin,
-      ValidRoles.treasure,
-      ValidRoles.president,
-      ValidRoles.user,
-    ],
-  },
-  {
-    title: "Crear Socio",
-    href: "/dashboard/member/new",
-    icon: "userPlus",
-    label: "user",
-    validRole: [ValidRoles.admin, ValidRoles.treasure],
-  },
-  {
-    title: "Socios",
-    href: "/dashboard/members",
-    icon: "user",
-    label: "user",
-    validRole: [ValidRoles.admin, ValidRoles.treasure],
-  },
-  {
-    title: "Socios Sin Pagar",
-    href: "/dashboard/members",
-    icon: "employee",
-    label: "employee",
-    validRole: [ValidRoles.admin, ValidRoles.treasure],
-  },
-  {
-    title: "Exitud",
-    href: "/dashboard/members",
-    icon: "cross",
-    label: "exitud",
-    validRole: [ValidRoles.admin, ValidRoles.treasure],
-  },
-  {
-    title: "Tareas",
-    href: "/dashboard/kanban",
-    icon: "kanban",
-    label: "kanban",
-    validRole: [
-      ValidRoles.admin,
-      ValidRoles.treasure,
-      ValidRoles.president,
-      ValidRoles.user,
-    ],
-  },
-  {
-    title: "Gestión de Calendario",
-    href: "/dashboard/calendar",
-    icon: "calendar",
-    label: "calendar",
-    validRole: [ValidRoles.admin, ValidRoles.president, ValidRoles.treasure],
-  },
-  {
-    title: "Socios por Banco",
-    href: "/dashboard/calendar",
-    icon: "bank",
-    label: "bank",
-    validRole: [ValidRoles.admin, ValidRoles.president],
-  },
+	{
+		title: "Athenea",
+		href: "/dashboard",
+		icon: "dashboard",
+		label: "Dashboard",
+		validRole: [ValidRoles.admin, ValidRoles.treasure, ValidRoles.president, ValidRoles.user],
+	},
+	{
+		title: "Crear Socio",
+		href: "/dashboard/member/new",
+		icon: "userPlus",
+		label: "user",
+		validRole: [ValidRoles.admin, ValidRoles.treasure],
+	},
+	{
+		title: "Socios",
+		href: "/dashboard/members",
+		icon: "user",
+		label: "user",
+		validRole: [ValidRoles.admin, ValidRoles.treasure],
+	},
+	{
+		title: "Socios Sin Pagar",
+		href: "/dashboard/members",
+		icon: "employee",
+		label: "employee",
+		validRole: [ValidRoles.admin, ValidRoles.treasure],
+	},
+	{
+		title: "Exitud",
+		href: "/dashboard/members",
+		icon: "cross",
+		label: "exitud",
+		validRole: [ValidRoles.admin, ValidRoles.treasure],
+	},
+	{
+		title: "Tareas",
+		href: "/dashboard/kanban",
+		icon: "kanban",
+		label: "kanban",
+		validRole: [ValidRoles.admin, ValidRoles.treasure, ValidRoles.president, ValidRoles.user],
+	},
+	{
+		title: "Gestión de Calendario",
+		href: "/dashboard/calendar",
+		icon: "calendar",
+		label: "calendar",
+		validRole: [ValidRoles.admin, ValidRoles.president, ValidRoles.treasure],
+	},
+	{
+		title: "Socios por Banco",
+		href: "/dashboard/calendar",
+		icon: "bank",
+		label: "bank",
+		validRole: [ValidRoles.admin, ValidRoles.president],
+	},
 
-  {
-    title: "Salir",
-    href: "/",
-    icon: "login",
-    label: "logout",
-    validRole: [
-      ValidRoles.admin,
-      ValidRoles.treasure,
-      ValidRoles.president,
-      ValidRoles.user,
-    ],
-  },
+	{
+		title: "Salir",
+		icon: "login",
+		label: "logout",
+		validRole: [ValidRoles.admin, ValidRoles.treasure, ValidRoles.president, ValidRoles.user],
+		onClick: (e) => {
+			e.preventDefault();
+			useAuthStore.getState().logoutUser();
+		},
+	},
 ];
