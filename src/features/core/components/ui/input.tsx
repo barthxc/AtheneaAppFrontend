9163754ef62665f5cc16b1f364dd6b0ed4d8683f
Mind <alwl1@hotmail.com>
2024-07-24 16:13:43 -0300

    fix: uncontrolled input value

--- src/features/core/components/ui/input.tsx
+++ src/features/core/components/ui/input.tsx
@@ -5,7 +5,7 @@ import { cn } from "@/features/core/lib/utils";
 export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
-	props.value = props.value ?? undefined;
+	props.value = props.value ?? "";
 	return (
 		<input
 			type={type}
