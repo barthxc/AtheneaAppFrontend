eebc3b3ee89ecba224b2c0989c885b0f9bd37c25
FOWMind <alwl1@hotmail.com>
2024-08-09 14:02:14 -0300

    style(landing): minimal corrections

--- src/features/landing/pages/landing.page.tsx
+++ src/features/landing/pages/landing.page.tsx
@@ -32,41 +32,12 @@ export function LandingPage() {
 			</h1>
 			<Footer />*/}
 
-			<header className="flex justify-start items-stretch">
-				<section className="bg-[#F3D047] flex-1 flex flex-col gap-20 py-5 px-12">
-					<div>
+			<header>
+				<div className="flex justify-between items-center absolute top-0 left-0 w-full h-20">
+					<div className="flex-1 flex justify-start items-center px-12 h-full">
 						<h2 className="text-2xl font-semibold">Athenea</h2>
 					</div>
-
-					<div className="flex flex-col gap-10">
-						<article className="flex flex-col gap-4">
-							<h3 className="text-2xl font-semibold">Donate Now</h3>
-							<p>
-								Your financial contribution can make a real difference. Every dollar you donate goes directly toward our
-								projects.
-							</p>
-							<Link to="#" className="font-medium text-base">
-								Get started
-							</Link>
-						</article>
-
-						<Separator className="bg-black" />
-
-						<article className="flex flex-col gap-4">
-							<h3 className="text-2xl font-semibold">Become a Volunteer</h3>
-							<p>
-								Join our team of dedicated volunteers and actively participate in our projects and events. Your time and
-								skills are invaluable.
-							</p>
-							<Link to="#" className="font-medium text-base">
-								Get started
-							</Link>
-						</article>
-					</div>
-				</section>
-
-				<section className="flex-[2.5] flex flex-col gap-14 py-5 px-12">
-					<nav className="flex justify-between items-center">
+					<nav className="flex-[2] px-12 flex justify-between items-center h-full">
 						<ul className="flex justify-start items-center gap-10 text-white">
 							<li>
 								<Link to="/">Home</Link>
@@ -88,10 +59,42 @@ export function LandingPage() {
 							Donate
 						</Link>
 					</nav>
+				</div>
 
-					<h2 className="text-white text-9xl font-display max-w-4xl">Changes Lives for the Better</h2>
-					<img src="/hero.jpg" alt="" className="w-full max-w-screen-md h-96 object-cover" />
-				</section>
+				<div className="flex justify-between items-stretch [&>*]:pt-24 [&>*]:pb-20">
+					<section className="bg-[#FFE353] flex-1 flex flex-col gap-20 px-12">
+						<div className="flex flex-col gap-10">
+							<article className="flex flex-col gap-4">
+								<h3 className="text-2xl font-semibold">Donate Now</h3>
+								<p>
+									Your financial contribution can make a real difference. Every dollar you donate goes directly toward
+									our projects.
+								</p>
+								<Link to="#" className="font-medium text-base">
+									Get started
+								</Link>
+							</article>
+
+							<Separator className="bg-black" />
+
+							<article className="flex flex-col gap-4">
+								<h3 className="text-2xl font-semibold">Become a Volunteer</h3>
+								<p>
+									Join our team of dedicated volunteers and actively participate in our projects and events. Your time
+									and skills are invaluable.
+								</p>
+								<Link to="#" className="font-medium text-base">
+									Get started
+								</Link>
+							</article>
+						</div>
+					</section>
+
+					<section className="flex-[2] flex flex-col gap-14 px-12">
+						<h2 className="text-white text-9xl font-display max-w-4xl">Changing Lives for the Better</h2>
+						<img src="/hero.jpg" alt="" className="w-full max-w-screen-md h-[500px] object-cover" />
+					</section>
+				</div>
 			</header>
 
 			<main>
