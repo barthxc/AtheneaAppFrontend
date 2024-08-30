f0081200cb4f2c4d087fd2ceac310620374b2ce8
FOWMind <alwl1@hotmail.com>
2024-08-30 00:57:56 -0300

    feat(dashboard): event delete core functionality

--- src/features/content/components/events/events.component.tsx
+++ src/features/content/components/events/events.component.tsx
@@ -11,12 +11,13 @@ import {
 	Heading,
 	Skeleton,
 } from "@/features/core/components/ui";
-import { useGetContents } from "@/features/content/hooks";
+import { useDeleteContent, useGetContents } from "@/features/content/hooks";
 import { EventsContent, EventsImages } from "@/features/content/components";
 import type { ContentResponse } from "@/features/content/types";
 
 export function Events() {
 	const { data: events, isLoading } = useGetContents();
+	const { mutate: deleteContent } = useDeleteContent();
 
 	const [eventList, eventContent, setEvents] = useDragAndDrop<HTMLDivElement, ContentResponse>(events ?? [], {
 		group: "eventList",
@@ -33,6 +34,17 @@ export function Events() {
 		setEvents(events);
 	}, [events, setEvents]);
 
+	const handleDeleteEvent = (eventId: string) => {
+		console.log(`DELETING EVENT WITH ID ${eventId}`);
+		// ! BE CAREFUL, IT WILL DELETE THE EVENT WITHOUT A CONFIRM DIALOG
+		// ! BE CAREFUL, IT WILL DELETE THE EVENT WITHOUT A CONFIRM DIALOG
+		// ! BE CAREFUL, IT WILL DELETE THE EVENT WITHOUT A CONFIRM DIALOG
+		// ! BE CAREFUL, IT WILL DELETE THE EVENT WITHOUT A CONFIRM DIALOG
+		// ! BE CAREFUL, IT WILL DELETE THE EVENT WITHOUT A CONFIRM DIALOG
+		// ! BE CAREFUL, IT WILL DELETE THE EVENT WITHOUT A CONFIRM DIALOG
+		// deleteContent(eventId);
+	};
+
 	if (isLoading) {
 		return [...new Array(2)].map((_, i) => (
 			<Skeleton key={Symbol(`event_skeleton_key_${i}`).description} className="w-full h-14 rounded-lg max-w-2xl" />
@@ -43,8 +55,13 @@ export function Events() {
 		<Accordion ref={eventList} type="single" collapsible className="flex flex-col gap-5">
 			{eventContent.length > 0 &&
 				eventContent.map((event) => (
-					<AccordionItem key={event.id} value={event.id} className="bg-secondary px-5 rounded-lg max-w-2xl">
-						<div className="flex justify-start items-center gap-5">
+					<AccordionItem key={event.id} value={event.id} className="bg-secondary rounded-lg overflow-hidden max-w-2xl">
+						<div className="flex justify-end items-center gap-5 w-full bg-primary">
+							<button type="button" className="p-px" onClick={() => handleDeleteEvent(event.id)}>
+								<Icons.close className="text-white" />
+							</button>
+						</div>
+						<div className="flex justify-start items-center gap-5 px-5">
 							<Icons.drag className="cursor-move drag-handler" />
 							<div className="w-full">
 								<AccordionTrigger className="flex justify-between items-center">
@@ -56,7 +73,7 @@ export function Events() {
 								</AccordionTrigger>
 							</div>
 						</div>
-						<AccordionContent className="[&_h2]:text-lg flex flex-col gap-5">
+						<AccordionContent className="[&_h2]:text-lg flex flex-col gap-5 px-5">
 							<EventsImages contentId={event.id} images={event.images} />
 							<EventsContent
 								contentId={event.id}
